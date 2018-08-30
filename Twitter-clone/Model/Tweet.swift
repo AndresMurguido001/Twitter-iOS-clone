94ca832ba298232f490f4d2ef3670860f495ce58
Your Name <dre.murguido@gmail.com>
2018-08-30 18:31:06 -0400

    added Tweet cell and tweet model

---
+++ Twitter-clone/Model/Tweet.swift
@@ -0,0 +1,9 @@
+//
+//  Tweet.swift
+//  Twitter-clone
+//
+//  Created by andres murguido on 8/30/18.
+//  Copyright © 2018 andres murguido. All rights reserved.
+//
+
+import Foundation
---
+++ Twitter-clone/View/TweetCell.swift
@@ -0,0 +1,9 @@
+//
+//  TweetCell.swift
+//  Twitter-clone
+//
+//  Created by andres murguido on 8/30/18.
+//  Copyright © 2018 andres murguido. All rights reserved.
+//
+
+import Foundation
